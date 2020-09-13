1876c9bce1734c7e36743f4e47707fea19bccf8c
Jing Li <thyrlian@gmail.com>
2020-09-13 10:33:41 +0200

    Add eureka-service skeleton

---
+++ Microservices/eureka-service/src/test/kotlin/com/basgeekball/eurekaservice/EurekaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.basgeekball.eurekaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
