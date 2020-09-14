080b56569605288ee9ef849ad6e59a8dec0c08dc
Jing Li <thyrlian@gmail.com>
2020-09-14 04:09:37 +0200

    Enable Eureka discovery

--- Microservices/user-service/src/main/kotlin/com/basgeekball/userservice/UserServiceApplication.kt
+++ Microservices/user-service/src/main/kotlin/com/basgeekball/userservice/UserServiceApplication.kt
@@ -2,7 +2,9 @@ package com.basgeekball.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class UserServiceApplication
 
