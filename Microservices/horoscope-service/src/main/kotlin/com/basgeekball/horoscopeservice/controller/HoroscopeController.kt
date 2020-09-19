9f579ab46fabbe106230e7b3fa7cb17df6097196
Jing Li <thyrlian@gmail.com>
2020-09-19 18:58:35 +0200

    Change HoroscopeController to return JSON response

--- Microservices/horoscope-service/src/main/kotlin/com/basgeekball/horoscopeservice/controller/HoroscopeController.kt
+++ Microservices/horoscope-service/src/main/kotlin/com/basgeekball/horoscopeservice/controller/HoroscopeController.kt
@@ -3,6 +3,8 @@ package com.basgeekball.horoscopeservice.controller
 import com.basgeekball.horoscopeservice.Horoscope
 import com.basgeekball.horoscopeservice.client.UserClient
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,10 +13,10 @@ class HoroscopeController() {
     @Autowired
     private lateinit var userClient: UserClient
 
-    @GetMapping("/fortune")
-    fun fortune(): String {
+    @GetMapping(value = ["/fortune"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun fortune(): ResponseEntity<String> {
         val user = userClient.getUsers().content.random()
         val zodiacSign = Horoscope.tellZodiacSign(user.dob)
-        return "${user.name} who was born on ${user.dob}, her/his zodiac sign is ${zodiacSign!!.symbol()}."
+        return ResponseEntity.ok("{\"name\": \"${user.name}\", \"dob\": \"${user.dob}\", \"zodiacSign\": {\"name\": \"${zodiacSign!!.name}\", \"symbol\": \"${zodiacSign!!.symbol()}\"}}")
     }
 }
