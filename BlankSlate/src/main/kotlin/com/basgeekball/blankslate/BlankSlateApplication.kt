0f07f6c42a2c0c4f2f4c9aa3aa0cfab9ae3af918
Jing Li <thyrlian@gmail.com>
2022-04-11 15:12:26 +0200

    Add a blank slate Spring web project

---
+++ BlankSlate/src/main/kotlin/com/basgeekball/blankslate/BlankSlateApplication.kt
@@ -0,0 +1,11 @@
+package com.basgeekball.blankslate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlankSlateApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlankSlateApplication>(*args)
+}
---
+++ BlankSlate/src/test/kotlin/com/basgeekball/blankslate/BlankSlateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.basgeekball.blankslate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlankSlateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
